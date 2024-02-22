0c62fbe1a8605d955e82cc0bf65fc5ae70bff62a
iireeneeps012 <ireperezsant@outlook.es>
2024-02-22 22:45:47 +0100

    Creadas dos entidades role y user, user no esta acabado

---
+++ src/main/java/com/codeUrjc/daw/Entity/Role.java
@@ -0,0 +1,59 @@
+package com.codeUrjc.daw.Entity;
+
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Role implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private Long id;
+
+    @Column
+    private String name;
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Column
+    private String description;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role role)) return false;
+        return Objects.equals(getId(), role.getId()) && Objects.equals(getName(), role.getName()) && Objects.equals(getDescription(), role.getDescription());
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getDescription());
+    }
+}
