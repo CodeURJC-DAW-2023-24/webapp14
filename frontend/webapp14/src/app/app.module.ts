48909a396d69e7438801539a38e7432bc7493d68
almuLR <a.lopezro.2021@alumnos.urjc.es>
2024-04-25 16:58:51 +0200

    Creamos la organización de componentes servisio y models

--- frontend/webapp14/src/app/app.module.ts
+++ frontend/webapp14/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {EventsComponent} from './components/events/events.component';
+import {UsersComponent} from './components/users/users.component';
+import {CommentsComponent} from './components/comments/comments.component';
+import {TicketsComponent} from  './components/tickets/tickets.component';
+import {HttpClientModule} from  '@angular/common/http';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    EventsComponent,
+    UsersComponent,
+    CommentsComponent,
+    TicketsComponent,
+    HttpClientModule
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
