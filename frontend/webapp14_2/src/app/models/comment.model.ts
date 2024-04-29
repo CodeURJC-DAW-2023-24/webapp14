f221460ee531b6d0e33fa8f38dc5a8a182633f35
AlbitaVM <albavm2003@gmail.com>
2024-04-29 18:00:37 +0200

    Clase model comentario  y empezado service

---
+++ frontend/webapp14_2/src/app/models/comment.model.ts
@@ -0,0 +1,6 @@
+export interface Comment {
+  id: number;
+  description: string;
+  nick: string;
+  event: Event;
+}
--- frontend/webapp14_2/src/app/services/comment.service.ts
+++ frontend/webapp14_2/src/app/services/comment.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {HttpClient,HttpErrorResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Comment} from "../models/comment.model";
 
 @Injectable({
   providedIn: 'root'
