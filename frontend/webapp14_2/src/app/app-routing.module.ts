b726fc556affc0b4561bcf449e7e80928487a74d
iireeneeps012 <ireperezsant@outlook.es>
2024-05-02 20:50:06 +0200

    se pasa error.component.html

--- frontend/webapp14_2/src/app/app-routing.module.ts
+++ frontend/webapp14_2/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageComponent } from './page/page.component';
-import {LoginComponent} from "./templates/login/login.component";
-import {RegisterComponent} from "./templates/register/register.component";
-import {ReviewComponent} from "./templates/review/review.component";
-import {ProfileComponent} from "./templates/profile/profile.component";
+import { LoginComponent } from "./templates/login/login.component";
+import { RegisterComponent } from "./templates/register/register.component";
+import { ReviewComponent } from "./templates/review/review.component";
+import { ProfileComponent } from "./templates/profile/profile.component";
+import { ErrorComponent } from "./templates/error/error.component";
 
 
 const routes: Routes = [
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path:'loginPage',component: LoginComponent },
   { path: 'registerPage', component: RegisterComponent },
   { path:'review', component:ReviewComponent},
-  { path:'profile',component:ProfileComponent}
+  { path:'profile',component:ProfileComponent},
+  {path:'error', component:ErrorComponent}
 ];
 
 @NgModule({
