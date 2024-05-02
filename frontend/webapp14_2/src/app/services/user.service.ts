f0c84fca204ae4b5b79d3aec66aa7050b55ec62b
almuLR <a.lopezro.2021@alumnos.urjc.es>
2024-05-02 19:40:06 +0200

    Graficos contadores

--- frontend/webapp14_2/src/app/services/user.service.ts
+++ frontend/webapp14_2/src/app/services/user.service.ts
@@ -23,4 +23,8 @@ export class UserService {
     return this.http.post<User>(`/api/users/`, dataUser);
   }
 
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`/api/users/`);
+  }
+
 }
--- frontend/webapp14_2/src/app/templates/dashboard/dashboard.component.ts
+++ frontend/webapp14_2/src/app/templates/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {User} from "../../models/user.model";
+import {UserService} from "../../services/user.service";
+import {EventService} from "../../services/event.service";
+import { Event } from "../../models/event.model";
 
 @Component({
   selector: 'app-dashboard',
@@ -11,5 +15,54 @@ import { Component } from '@angular/core';
     ]
 })
 export class DashboardComponent {
+  numUsers: number = 0;
+  numEvents: number = 0;
+  countTecnologias: number = 0;
+  countArtes: number = 0;
+  countSanitarios: number = 0;
+  countHumanidades: number = 0;
+
+  constructor(private userService: UserService, private eventService:EventService) {}
+
+  ngOnInit(): void {
+    this.eventService.getEvents().subscribe(
+      (events:Event[]) => {
+        this.numEvents = events.length;
+      }
+    )
+
+    this.userService.getUsers().subscribe(
+      (users:User[]) => {
+        this.numUsers = users.length;
+      }
+    )
+
+    this.eventService.getTecnologia().subscribe(
+      (tecnology: number) => {
+        this.countTecnologias = tecnology;
+      }
+    )
+
+    this.eventService.getArte().subscribe(
+      (arte: number) => {
+        this.countArtes = arte;
+      }
+    )
+
+    this.eventService.getSanitario().subscribe(
+      (sanitario: number) => {
+        this.countSanitarios = sanitario;
+      }
+    )
+
+    this.eventService.getHumanidades().subscribe(
+      (humanidades: number) => {
+        this.countHumanidades = humanidades;
+      }
+    )
+
+
+
+  }
 
 }
