5ea6d5574fdfd31109ddbfd746298990b7ca02c5
almuLR <a.lopezro.2021@alumnos.urjc.es>
2024-05-03 09:58:42 +0200

    Dashboard al completo

--- frontend/webapp14_2/src/app/app-routing.module.ts
+++ frontend/webapp14_2/src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import {LoginComponent} from "./templates/login/login.component";
 import {RegisterComponent} from "./templates/register/register.component";
 import {ReviewComponent} from "./templates/review/review.component";
 import {DashboardComponent} from "./templates/dashboard/dashboard.component";
+import {ProfileComponent} from "./templates/profile/profile.component";
+import {EventsComponent} from "./templates/events/events.component";
+import {UserPermissionsComponent} from "./templates/user-permissions/user-permissions.component";
 
 
 const routes: Routes = [
@@ -13,6 +16,9 @@ const routes: Routes = [
   { path: 'registerPage', component: RegisterComponent },
   {path:'reviewPage', component:ReviewComponent},
   { path:'dashboardPage', component: DashboardComponent },
+  { path:'profilePage', component: ProfileComponent },
+  { path:'eventsPage', component: EventsComponent },
+  { path:'permissionPage', component: UserPermissionsComponent },
 ];
 
 @NgModule({
--- frontend/webapp14_2/src/app/templates/dashboard/dashboard.component.ts
+++ frontend/webapp14_2/src/app/templates/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
-import {User} from "../../models/user.model";
-import {UserService} from "../../services/user.service";
-import {EventService} from "../../services/event.service";
+import { Component, OnInit } from '@angular/core';
+import { User } from "../../models/user.model";
+import { UserService } from "../../services/user.service";
+import { EventService } from "../../services/event.service";
 import { Event } from "../../models/event.model";
+import { Chart } from 'chart.js/auto';
+import { forkJoin } from 'rxjs';
+import { Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -12,57 +15,112 @@ import { Event } from "../../models/event.model";
     '../../../assets/css/nucleo-icons.css',
     '../../../assets/css/nucleo-svg.css',
     '../../../assets/scss/material-dashboard.scss',
-    ]
+  ]
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   numUsers: number = 0;
   numEvents: number = 0;
   countTecnologias: number = 0;
   countArtes: number = 0;
   countSanitarios: number = 0;
   countHumanidades: number = 0;
+  dataLoaded: boolean = false;
+  isAdmin: boolean = false;
+  isEditor: boolean = false;
 
-  constructor(private userService: UserService, private eventService:EventService) {}
+  constructor(private userService: UserService, private eventService: EventService, private router: Router) {}
 
   ngOnInit(): void {
-    this.eventService.getEvents().subscribe(
-      (events:Event[]) => {
+    forkJoin([
+      this.eventService.getEvents(),
+      this.userService.getUsers(),
+      this.eventService.getTecnologia(),
+      this.eventService.getArte(),
+      this.eventService.getSanitario(),
+      this.eventService.getHumanidades()
+    ]).subscribe(
+      ([events, users, tecnologia, arte, sanitario, humanidades]: [Event[], User[], number, number, number, number]) => {
         this.numEvents = events.length;
-      }
-    )
-
-    this.userService.getUsers().subscribe(
-      (users:User[]) => {
         this.numUsers = users.length;
-      }
-    )
-
-    this.eventService.getTecnologia().subscribe(
-      (tecnology: number) => {
-        this.countTecnologias = tecnology;
-      }
-    )
-
-    this.eventService.getArte().subscribe(
-      (arte: number) => {
+        this.countTecnologias = tecnologia;
         this.countArtes = arte;
-      }
-    )
-
-    this.eventService.getSanitario().subscribe(
-      (sanitario: number) => {
         this.countSanitarios = sanitario;
-      }
-    )
-
-    this.eventService.getHumanidades().subscribe(
-      (humanidades: number) => {
         this.countHumanidades = humanidades;
+        this.dataLoaded = true;
+        this.initializeChart();
+      }
+    );
+  }
+
+  initializeChart(): void {
+    if (!this.dataLoaded) return;
+
+    // Calcular los datos para el gráfico de quesitos
+    const labels = ['Tecnología', 'Artes', 'Sanitario', 'Humanidades'];
+    const data = [this.countTecnologias, this.countArtes, this.countSanitarios, this.countHumanidades];
+
+    // Obtener el elemento canvas
+    const ctxPie = document.getElementById("chart-pie") as HTMLCanvasElement;
+
+    if (ctxPie) {
+      new Chart(ctxPie, {
+        type: "pie",
+        data: {
+          labels: labels,
+          datasets: [{
+            label: "Eventos por Categoría",
+            data: data,
+            backgroundColor: [
+              'rgb(204,165,165)',
+              'rgb(91,57,57)',
+              'rgb(145,49,49)',
+              'rgb(73,9,9)'
+            ],
+            borderColor: [
+              'rgb(204,165,165)',
+              'rgb(91,57,57)',
+              'rgb(145,49,49)',
+              'rgb(73,9,9)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      });
+    }
+    this.userService.getCurrentUser().subscribe(
+      (user: User) => {
+        this.isEditor = user.editor;
+        this.isAdmin = user.roles.indexOf('ADMIN') > -1;
       }
     )
+  }
 
+  goToIndex(){
+    this.router.navigate(['/']);
+  }
 
+  goToDashboard(){
+    this.router.navigate(['/dashboardPage']);
+  }
 
+  goToReview() {
+    this.router.navigate(['/reviewPage']);
+  }
+
+  goToUserPermissions(){
+    this.router.navigate(['/permissionPage']);
+  }
+
+  goToProfile(){
+    this.router.navigate(['/profilePage']);
+  }
+
+  goToEvents(){
+    this.router.navigate(['/eventsPage']);
   }
 
 }
